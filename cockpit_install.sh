9a1fd087f74081dad649a1ddd9c320e8722df89b
cbritos2016 <cristian@britos.net>
2022-06-20 15:22:32 -0400

    Update cockpit_install.sh

--- cockpit_install.sh
+++ cockpit_install.sh
@@ -1,6 +1,6 @@
-sudo dnf install cockpit
-sudo dnf install cockpit cockpit-machines pcp cockpit-pcp packagekit virt-viewer 
-sudo dnf install cockpit cockpit-machines pcp cockpit-pcp virt-viewer 
+#sudo dnf install cockpit
+sudo dnf install cockpit cockpit-machines pcp cockpit-pcp packagekit virt-viewer -y
+sudo dnf install cockpit cockpit-machines pcp cockpit-pcp virt-viewer -y
 sudo systemctl enable --now cockpit.socket
 sudo systemctl enable --now pmcd 
 sudo systemctl enable --now pmlogger
